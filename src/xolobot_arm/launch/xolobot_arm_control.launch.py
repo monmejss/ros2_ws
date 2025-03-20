d7460d22a8a973dddfe8e637df8d9b763cc23a51
monmejss <mbetsabe02@gmail.com>
2025-03-20 14:13:15 -0600

    Correccion de errores en launch

--- src/xolobot_arm/launch/xolobot_arm_control.launch.py
+++ src/xolobot_arm/launch/xolobot_arm_control.launch.py
@@ -1,62 +1,75 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
+
+from launch.actions import LogInfo, ExecuteProcess
 
 def generate_launch_description():
-    pkg_xolobot_arm = get_package_share_directory('xolobot_arm')
-    pkg_xolobot_control = get_package_share_directory('xolobot_control')
-
-    sdf_file = os.path.join(pkg_xolobot_arm, 'models', 'xolobot_arm.sdf')
-    world_file = os.path.join(pkg_xolobot_arm, 'worlds', 'coca_levitando.world')
-    urdf_file = os.path.join(pkg_xolobot_arm, 'models', 'xolobot.urdf')
+    # Paquetes necesarios
+    package_name1 = "xolobot_arm"
+    package_name2 = "xolobot_control"
     
-    with open(urdf_file, 'r') as file:
-        robot_description_content = file.read()
-
-    gazebo_ros_launch = os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+    # coca_levitando.world
+    world_path = os.path.join(
+        FindPackageShare(package_name1).find(package_name1), 
+        "worlds", "coca_levitando.world"
+    )
+    
+    # xolobot_control.yaml
+    config_path = os.path.join(
+        FindPackageShare(package_name2).find(package_name2), 
+        "config", "xolobot_control.yaml"
+    )
+    
+    # xolobot_arm.sdf. Lo quite de aqui porque esta en el world
+    #sdf_path = os.path.join(
+    #    FindPackageShare(package_name1).find(package_name1), 
+    #    "models", "xolobot_arm.sdf"
+    #)
+    
+    # xolobot.urdf
+    urdf_path = os.path.join(
+        FindPackageShare(package_name1).find(package_name1), 
+        "models", "xolobot.urdf"
+    )
+    
+    # Iniciar Gazebo Ignition con el modelo SDF
+    gazebo_launch = ExecuteProcess(
+        cmd=['ign', 'gazebo', world_path, '-v', '4', '--gui'],
+        output='screen'
+    )
+        
+    with open(urdf_path, 'r') as file:
+        descripcion_xolobot = file.read()
 
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gazebo_ros_launch),
-            launch_arguments={'world': world_file}.items()
-        ),
+        gazebo_launch,
         
-        Node(
-            package='ros_gz_sim',
-            executable='create',
-            arguments=['-file', sdf_file, '-name', 'xolobot_arm', '-topic', '/robot_description'],
-            output='screen'
-        ),
-        
+        # Este nodo utiliza la descripcion del URDF
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            parameters=[{'robot_description': robot_description_content}],
+            parameters=[{'robot_description': descripcion_xolobot}],
             output='screen'
         ),
 
         Node(
             package='controller_manager',
             executable='ros2_control_node',
-            parameters=[{os.path.join(pkg_xolobot_control, 'config', 'xolobot_control.yaml')}, 
-                        {'hardware_interface': 'gz_ros2_control/GazeboSystemInterface'}],
+            parameters=[{config_path},{'gazebo_ros2_control': 'gazebo_ros2_control/GazeboSystemInterface'}],
             output='screen'
         ),
         
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['arm_controller'],
+        # joint_trajectory_controller
+        ExecuteProcess(
+            cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_trajectory_controller'],
             output='screen'
         ),
         
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['joint_state_broadcaster'],
+        # joint_state_broadcaster
+        ExecuteProcess(
+            cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_state_broadcaster'],
             output='screen'
         ),
     ])
